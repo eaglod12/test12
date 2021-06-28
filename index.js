0ac6026afb1d0efd47650c440e4929533dae7d52
franck leleux <franckl2012@gmail.com>
2021-06-28 14:04:06 +0200

    Affichage des articles avec Js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ async function main() {
 
 function getArticles() {
     return fetch("http://localhost:3000/api/teddies")
-        .then(function (httpBodyResponse) {
-            return httpBodyResponse.json()
+        .then(function (res) {
+            return res.json()
         })
         .then(function (articles) {
             return articles
@@ -25,13 +25,16 @@ function getArticles() {
 function displayArticle(article) {
     const templateElt = document.getElementById("produit");
     let vueArticle = `        
-
+    <figure>
+        <img src="`+article.imageUrl+`" alt="`+article.name+`" width="400" height="300">
+        <figcaption>`+article.name+`</figcaption>
+        <figcaption>`+article.description+`</figcaption>
+        <figcaption>`+article.price+`</figcaption>
+    </figure>
 `
     console.log(article['imageUrl']);
     templateElt.insertAdjacentHTML('afterend', vueArticle);
 }
-
-
 
 
 
