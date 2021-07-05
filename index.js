d5c37ab32ed26f97830098176fdbc3cd4c642484
franck leleux <franckl2012@gmail.com>
2021-07-05 10:35:41 +0200

    mise de commentaires pour le code

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ main()
 
 async function main() {
     const articles = await getArticles();
-    console.log(articles); // Affichage de array pour vérifier si fetch répond
+    console.log(articles); // Affichage de array dans la console pour vérifier si fetch répond
     for (article of articles) {
-        displayArticle(article);
+        displayArticle(article); //display défini le type d'affichage,article l'élément de contenu
     }
 
 }
@@ -27,7 +27,7 @@ function getArticles() {
 //function pour récuperer et afficher les articles sur la page d'acceuil
 
 function displayArticle(article) {
-    const templateElt = document.getElementById("produit");
+    const element = document.getElementById("produit");
     let vueArticle = `       
     <figure id="encart-art" class= "encart-art">
         <a href="produit.html" id="link-produit" class="link-produit"/>
@@ -36,10 +36,14 @@ function displayArticle(article) {
         <figcaption class="description">`+ article.description + `</figcaption>
         <figcaption class="price">`+ article.price + `</figcaption>
     </figure>`
-    console.log(article['imageUrl']); //pour la visualisation des Url
-    templateElt.insertAdjacentHTML('afterend', vueArticle);
+    console.log(article['imageUrl']); //pour la visualisation des Url dans la console
+    element.insertAdjacentHTML('afterend', vueArticle);
 }
-
+    /*insertAdjacentHTML() analyse le texte spécifié en tant que HTML ou XML et
+     insère les noeuds résultants dans le DOM à la position spécifiée. 
+     L'élement qui est utilisé n'est pas  réanalysé et les élements qu'il contient 
+     ne sont donc pas corrompus. Ceci, et le fait d'éviter la sérialisation 
+     supplémentaire, rend la fonction plus rapide et directe que innerHTML.*/
 
 
 
