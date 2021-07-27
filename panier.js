ba2cbbe25ff8b3f806a6706aa155ef9dad79ab81
franck leleux <franckl2012@gmail.com>
2021-07-27 20:50:59 +0200

    enregistrement du produit dans le panier

--- panier.js
+++ panier.js
@@ -1,106 +1,42 @@
 
 
-function ajouter() {
-    let code = parseInt(document.getElementById("id").value);
-    let qte = parseInt(document.getElementById("qte").value);
-    let prix = parseInt(document.getElementById("prix").value);
-    let monPanier = new Panier();
-    monPanier.ajouterArticle(code, qte, prix);
-    let tableau = document.getElementById("tableau");
-    let longueurTab = parseInt(document.getElementById("nbreLignes").innerHTML);
-    if (longueurTab > 0) {
-        for (let i = longueurTab; i > 0; i--) {
-            monPanier.ajouterArticle(parseInt(tableau.rows[i].cells[0].innerHTML), parseInt(tableau.rows[i].cells[1].innerHTML), parseInt(tableau.rows[i].cells[2].innerHTML));
-            tableau.deleteRow(i);
-        }
-    }
-    let longueur = monPanier.liste.length;
-    for (let i = 0; i < longueur; i++) {
-        let ligne = monPanier.liste[i];
-        let ligneTableau = tableau.insertRow(-1);
-        let colonne1 = ligneTableau.insertCell(0);
-        colonne1.innerHTML += ligne.getCode();
-        let colonne2 = ligneTableau.insertCell(1);
-        colonne2.innerHTML += ligne.qteArticle;
-        let colonne3 = ligneTableau.insertCell(2);
-        colonne3.innerHTML += ligne.prixArticle;
-        let colonne4 = ligneTableau.insertCell(3);
-        colonne4.innerHTML += ligne.getPrixLigne();
-        let colonne5 = ligneTableau.insertCell(4);
-        colonne5.innerHTML += "<button class=\"btn btn-primary\" type=\"submit\" onclick=\"supprimer(this.parentNode.parentNode.cells[0].innerHTML)\"><span class=\"glyphicon glyphicon-remove\"></span> Retirer</button>";
-    }
-    document.getElementById("prixTotal").innerHTML = monPanier.getPrixPanier();
-    document.getElementById("nbreLignes").innerHTML = longueur;
-}
-
-function supprimer(code) {
-    let monPanier = new Panier();
-    let tableau = document.getElementById("tableau");
-    let longueurTab = parseInt(document.getElementById("nbreLignes").innerHTML);
-    if (longueurTab > 0) {
-        for (let i = longueurTab; i > 0; i--) {
-            monPanier.ajouterArticle(parseInt(tableau.rows[i].cells[0].innerHTML), parseInt(tableau.rows[i].cells[1].innerHTML), parseInt(tableau.rows[i].cells[2].innerHTML));
-            tableau.deleteRow(i);
-        }
-    }
-    monPanier.supprimerArticle(code);
-    let longueur = monPanier.liste.length;
-    for (let i = 0; i < longueur; i++) {
-        let ligne = monPanier.liste[i];
-        let ligneTableau = tableau.insertRow(-1);
-        let colonne1 = ligneTableau.insertCell(0);
-        colonne1.innerHTML += ligne.getCode();
-        let colonne2 = ligneTableau.insertCell(1);
-        colonne2.innerHTML += ligne.qteArticle;
-        let colonne3 = ligneTableau.insertCell(2);
-        colonne3.innerHTML += ligne.prixArticle;
-        let colonne4 = ligneTableau.insertCell(3);
-        colonne4.innerHTML += ligne.getPrixLigne();
-        let colonne5 = ligneTableau.insertCell(4);
-        colonne5.innerHTML += "<button class=\"btn btn-primary\" type=\"submit\" onclick=\"supprimer(this.parentNode.parentNode.cells[0].innerHTML)\"><span class=\"glyphicon glyphicon-remove\"></span> Retirer</button>";
-    }
-    document.getElementById("prixTotal").innerHTML = monPanier.getPrixPanier();
-    document.getElementById("nbreLignes").innerHTML = longueur;
-}
-
-
 function Panier() {
     this.liste = [];
-    this.ajouterArticle = function (code, qte, prix) {
-        let index = this.getArticle(code);
-        if (index == -1) this.liste.push(new LignePanier(code, qte, prix));
-        else this.liste[index].ajouterQte(qte);
+    this.ajouterArticle = function (produit, quantite, price) {
+        let index = this.getArticle(produit);
+        if (index == -1) this.liste.push(new LignePanier(produit, quantite, price));
+        else this.liste[index].ajouterquantite(quantite);
     }
-    this.getPrixPanier = function () {
+    this.getpricePanier = function () {
         let total = 0;
         for (let i = 0; i < this.liste.length; i++)
-            total += this.liste[i].getPrixLigne();
+            total += this.liste[i].getpriceLigne();
         return total;
     }
-    this.getArticle = function (code) {
+    this.getArticle = function (produit) {
         for (let i = 0; i < this.liste.length; i++)
-            if (code == this.liste[i].getCode()) return i;
+            if (produit == this.liste[i].getproduit()) return i;
         return -1;
     }
-    this.supprimerArticle = function (code) {
-        let index = this.getArticle(code);
+    this.supprimerArticle = function (produit) {
+        let index = this.getArticle(produit);
         if (index > -1) this.liste.splice(index, 1);
     }
 }
 
-function LignePanier(code, qte, prix) {
-    this.codeArticle = code;
-    this.qteArticle = qte;
-    this.prixArticle = prix;
-    this.ajouterQte = function (qte) {
-        this.qteArticle += qte;
+function LignePanier(produit, quantite, price) {
+    this.produitArticle = produit;
+    this.quantiteArticle = quantite;
+    this.priceArticle = price;
+    this.ajouterquantite = function (quantite) {
+        this.quantiteArticle += quantite;
     }
-    this.getPrixLigne = function () {
-        let resultat = this.prixArticle * this.qteArticle;
+    this.getpriceLigne = function () {
+        let resultat = this.priceArticle * this.quantiteArticle;
         return resultat;
     }
-    this.getCode = function () {
-        return this.codeArticle;
+    this.getproduit = function () {
+        return this.produitArticle;
     }
 }
 
--- produit.js
+++ produit.js
@@ -25,24 +25,32 @@ function displayArticle(article) {
     const element = document.getElementById("article");
 
     let vueArticle = `  
-
+        
     <figure id="encart-art" class= "encart-art">
-        <img class="teddy-img" src="`+ article.imageUrl + ` "width=""height="">
+    <img class="teddy-img" src="`+ article.imageUrl + ` "width=""height="">
 
     <div class="info" id="info">     
         <figcaption class="name">`+ article.name + `</figcaption>
         <figcaption class="description">`+ article.description + `</figcaption>
         <figcaption class="price" id="price">` + article.price / 100 + ",00 €" + `</figcaption>
-        <label class="quantite-produit">Quantité</label><input type = "number" id = "qte" style= "width: 3.8rem" class="input-sm form-control"></input>
-        <button class="btn-card" class="btn btn-primary" type="submit" onclick="ajouter()"><span class="glyphicon glyphicon-shopping-cart"></span> Ajouter au panier</button>
+
+        <label id="quantite-produit" class="quantite-produit">Quantité</label><input type="number" id="quantite" style="width:4rem"
+        class="input-sm form-control"></input>
+
+        <button class="btn-card" class="btn btn-primary" type="submit" onclick="ajouter()"><span
+            class="glyphicon glyphicon-shopping-cart"></span> Ajouter au panier</button>
+
     </div>
+
 
     </figure>`
 
     element.insertAdjacentHTML('afterend', vueArticle);
 }
 
+
 /*-----function pour récupérer les paramétres GET d'une URL------*/
+
 
 function $_GET(param) {
     let lets = {};
@@ -59,6 +67,86 @@ function $_GET(param) {
     return lets;
 }
 
+    /*-----------------function pour ajouter au tableau------------------*/
+
+
+function ajouter() {
+    const articleId = $_GET("id");
+    let produit = articleId;
+    let quantite = parseInt(document.getElementById("quantite").value);
+    let price = parseInt(document.getElementById("price").innerHTML); console.log(document.getElementById("price").innerHTML);
+    let monPanier = new Panier();
+    console.log(quantite);
+    monPanier.ajouterArticle(produit, quantite, price);
+    let tableau = document.getElementById("tableau");
+    let longueurTab = parseInt(document.getElementById("quantite").value);
+    /*if (longueurTab > 0) {
+        for (let i = longueurTab; i > 0; i--) {
+            monPanier.ajouterArticle(articleId, price, quantite);
+            tableau.deleteRow(i);
+        }
+    }*/
+
+    /*-----------------création du tableau-----------------------*/
+
+
+    let longueur = monPanier.liste.length;
+    for (let i = 0; i < longueur; i++) {
+        let ligne = monPanier.liste[i];
+        console.log(tableau);
+        let ligneTableau = tableau.insertRow(-1);
+        let colonne1 = ligneTableau.insertCell(0);
+        colonne1.innerHTML += ligne.getproduit();
+        let colonne2 = ligneTableau.insertCell(1);
+        colonne2.innerHTML += ligne.quantiteArticle;
+        let colonne3 = ligneTableau.insertCell(2);
+        colonne3.innerHTML += ligne.priceArticle;
+        let colonne4 = ligneTableau.insertCell(3);
+        colonne4.innerHTML += ligne.getpriceLigne();
+        let colonne5 = ligneTableau.insertCell(4);
+        colonne5.innerHTML += "<button class=\"btn btn-primary\" type=\"submit\" onclick=\"supprimer(this.parentNode.parentNode.cells[0].innerHTML)\"><span class=\"glyphicon glyphicon-remove\"></span> Retirer</button>";
+    }console.log(document.getElementById("priceTotal"));
+    document.getElementById("priceTotal").innerHTML = monPanier.getpricePanier();
+    document.getElementById("nbreLignes").innerHTML = longueur;
+    console.log(document.getElementById("nb-article"));
+    document.getElementById("nb-article").innerHTML = quantite;
+}
+
+    /*----------function pour suprimer les lignes du tableau de commande---------*/
+
+
+function supprimer(produit) {
+    let monPanier = new Panier();
+    let tableau = document.getElementById("tableau");
+    let longueurTab = parseInt(document.getElementById("nbreLignes").innerHTML);
+    if (longueurTab > 0) {
+        for (let i = longueurTab; i > 0; i--) {
+            monPanier.ajouterArticle(parseInt(tableau.rows[i].cells[0].innerHTML), parseInt(tableau.rows[i].cells[1].innerHTML), parseInt(tableau.rows[i].cells[2].innerHTML));
+            tableau.deleteRow(i);
+        }
+    }
+    monPanier.supprimerArticle(produit);
+    let longueur = monPanier.liste.length;
+    for (let i = 0; i < longueur; i++) {
+        let ligne = monPanier.liste[i];
+        let ligneTableau = tableau.insertRow(-1);
+        let colonne1 = ligneTableau.insertCell(0);
+        colonne1.innerHTML += ligne.getproduit();
+        let colonne2 = ligneTableau.insertCell(1);
+        colonne2.innerHTML += ligne.quantiteArticle;
+        let colonne3 = ligneTableau.insertCell(2);
+        colonne3.innerHTML += ligne.priceArticle;
+        let colonne4 = ligneTableau.insertCell(3);
+        colonne4.innerHTML += ligne.getpriceLigne();
+        let colonne5 = ligneTableau.insertCell(4);
+        colonne5.innerHTML += "<button class=\"btn btn-primary\" type=\"submit\" onclick=\"supprimer(this.parentNode.parentNode.cells[0].innerHTML)\"><span class=\"glyphicon glyphicon-remove\"></span> Retirer</button>";
+    }
+    document.getElementById("priceTotal").innerHTML = monPanier.getpricePanier();
+    document.getElementById("nbreLignes").innerHTML = longueur;
+
+    console.log(document.getElementById("nb-article"));
+    document.getElementById("nb-article").innerHTML = parseInt(document.getElementById("priceTotal").innerHTML /(document.getElementById("price").innerHTML * document.getElementById("quantite").innerHTML));
+}
 
 
 
