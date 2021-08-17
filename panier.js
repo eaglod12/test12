e306e21bcfe07682908752b4fdfc0eac2db0cf62
franck leleux <franckl2012@gmail.com>
2021-08-17 16:43:43 +0200

    mise a jour des regex

--- panier.js
+++ panier.js
@@ -73,12 +73,12 @@ function displayPanier(monPanier) {
 
 }
 
-    // Formulaire de Commande
+// Formulaire de Commande
 
-    const afficherFormulaireHtml = () => {
-        // Sléction de l'élément du DOM pour la position du formulaire
-        const structureFormulaire = document.querySelector("#formulaire");
-        const formulaireHtml = `
+const afficherFormulaireHtml = () => {
+    // Sléction de l'élément du DOM pour la position du formulaire
+    const structureFormulaire = document.querySelector("#formulaire");
+    const formulaireHtml = `
             <div id="formulaire-commande">
                 <h2>Valider la commande en remplissant le formulaire</h2>
                 <form method="post" action= "http://localhost:3000/api/cameras"  name="validation" >
@@ -103,12 +103,120 @@ function displayPanier(monPanier) {
                 </form>
             </div>    
         `;
-        structureFormulaire.insertAdjacentHTML("afterend", formulaireHtml);
+    structureFormulaire.insertAdjacentHTML("afterend", formulaireHtml);
+};
+
+//appel function formulaire
+
+afficherFormulaireHtml();
+
+
+
+// Gestion du formulaire
+const btnValidationFormulaire = document.querySelector("#envoyerCommande");
+
+btnValidationFormulaire.addEventListener("click", (e) => {
+    e.preventDefault();
+    const validationFormulaire = true;
+    let erreur;
+    const inputs = document.getElementsByTagName("input");
+    
+
+    // Boucle pour remplir tous les champs du formulaire
+    for (let n = 0; n < inputs.length; n++) {
+        if (!inputs[n].value) {
+            erreur = "Veuillez compléter tous les champs du formulaire";
+            break;
+        }
     };
 
-    //appel function formolaire
+    // Message d'erreur si le formulaire n'est pas rempli dans la totalité
+    if (erreur) {
+        document.querySelector("#erreur").innerHTML = erreur;
+    };
 
-    afficherFormulaireHtml();
+    // Gestion des RegEx selon les différentes values du formulaire: Ex: pas de chiffres dans la ville
+    lastName = validation.lastName.value;
+    regExpLastName = /^[a-zA-Z\s]{3,10}$/;
+    if (regExpLastName.test(lastName) == false) {
+        alert("NOM: Seuls les lettres en majuscules et minuscules sont acceptées. 3 caractères minimum");
+        return false;
+    }
+
+    // RegEx pour le champ prenom
+    firstName = validation.firstName.value;
+    regExFirstName = /^[a-zA-Z\s]{3,10}$/;
+    if (regExFirstName.test(firstName) == false) {
+        alert("PRENOM: Les caractères spéciaux et les chiffres ne sont pas acceptés.");
+        return false;
+    }
+
+    // RegEx pour le champ email
+    email = validation.email.value;
+    regExMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (regExMail.test(email) == false) {
+        alert("EMAIL: Les caractères @ et . sont obligatoires.");
+        return false;
+    }
+
+    // RegEx du champ adresse
+    adress = validation.adress.value;
+    regExAdress = /^[a-zA-Z0-9\s]{10,25}$/;
+    if (regExAdress.test(adress) == false) {
+        alert("ADRESSSE: Minimum de 10 caractères demandés.");
+        return false;
+    }
+
+    // RegEx du champ ville
+    city = validation.city.value;
+    regExCity = /^[a-zA-Z\s]{5,15}$/;
+    if (regExCity.test(city) == false) {
+        alert("VILLE: Les chiffres ne sont pas autorisés. Minimum 5 caractères.");
+        return false;
+    }
+
+    if (validationFormulaire) { // Envoyer le formulaire si valide
+        alert("formulaire envoyé");
+    } else {
+        e.preventDefault();
+        alert("formulaire non envoyé");
+    }
+
+
+    /* Tableau produit à envoyer vers le serveur */
+    let products = [];
+    for (product of produitEnregistreDansLocalStorage) {
+        products.push(product.id);
+    }
+
+
+    /* Objet contact à envoyer vers le serveur */
+    let contact = {
+        firstName: document.querySelector("#firstName").value,
+        lastName: document.querySelector("#lastName").value,
+        address: document.querySelector("#adress").value,
+        city: document.querySelector("#city").value,
+        email: document.querySelector("#email").value,
+    };
+
+
+    /* Requête POST pour envoyer les produits + contact vers le serveur */
+
+    fetch("http://localhost:3000/api/cameras/order", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify({ contact, products, prixTotal }),
+    })
+        .then((response) => response.json())
+        .then((response) => {
+            localStorage.setItem("orderId", response.orderId);
+            console.log(response.orderId);
+            window.location = "confirmation.html";
+        })
+})
+
 
 
 
